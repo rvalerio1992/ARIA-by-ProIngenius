2f17c448bd08e821a1ad119c1f350faec00cda69
Rafael Valerio <rvalerio1992@gmail.com>
2026-01-08 12:24:46 -0600

    fix: bind to 0.0.0.0 for render deployment

--- server/index.ts
+++ server/index.ts
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  const host = process.env.HOST || "127.0.0.1"; // Use 127.0.0.1 for local development
+  const host = process.env.HOST || "0.0.0.0";
   server.listen(port, host, () => {
     log(`serving on http://${host}:${port}`);
   });
